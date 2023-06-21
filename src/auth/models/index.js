1551267309c57c3e05587eb0b4bbd61679a8ce96
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-06-21 12:13:44 +0000

    modifying

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -5,18 +5,18 @@ const userSchema= require('./users-model')
 
 // const POSTGRES_URI= process.env.NODE_ENV ==='test' ? "sqlite:memory" :process.env.DATABASE_URL;
 const POSTGRES_URI= process.env.DATABASE_URL;
-// let sequelizeOptions = process.env.NODE_ENV === "production" ?
-//     {
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false,
-//             },
-//         },
-//     } :
-//     {}
+let sequelizeOptions = process.env.NODE_ENV === "production" ?
+    {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    } :
+    {}
 
-    let sequelize = new Sequelize(POSTGRES_URI, {});
+    let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
 
 
     module.exports = {
